56d14e1fbb83ed75e742045312d1d9fd3dd6fe09
Maria Bredeleva <maria.yaikova@gmail.com>
2016-01-28 01:28:08 +0300

    Добавила еще класс для общих операций с wd

--- test_addgroup_rec.py
+++ test_addgroup_rec.py
@@ -2,6 +2,7 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
 from group import Group
+from wd_helper import WDH
 
 def is_alert_present(wd):
     try:
@@ -10,37 +11,38 @@ def is_alert_present(wd):
     except:
         return False
 
+
 class my_test(unittest.TestCase):
     def setUp(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
+        self.wd_helper = WDH(WebDriver())
+        self.wd_helper.wd.implicitly_wait(60)
     
     def test_add_group(self):
-        wd = self.wd
-        self.open_url(wd, "http://localhost/addressbook/")
-        self.login(wd, login="admin", pwd="secret")
-        self.open_group_page(wd)
-        self.add_group(wd, Group('lalala','lalala','lalala'))
-        self.return_to_group_page(wd)
-        self.logout(wd)
+        wd_helper = self.wd_helper
+        wd_helper.open_url("http://localhost/addressbook/")
+        wd_helper.login(login="admin", pwd="secret")
+        self.open_group_page(wd_helper.wd)
+        self.add_group(wd_helper.wd, Group('lalala','lalala','lalala'))
+        self.return_to_group_page(wd_helper.wd)
+        self.logout(wd_helper.wd)
 
     def test_add_empty_group(self):
-        wd = self.wd
-        self.open_url(wd, "http://localhost/addressbook/")
-        self.login(wd, login="admin", pwd="secret")
-        self.open_group_page(wd)
-        self.add_group(wd, Group('','',''))
-        self.return_to_group_page(wd)
-        self.logout(wd)
+        wd_helper = self.wd_helper
+        wd_helper.open_url("http://localhost/addressbook/")
+        wd_helper.login(login="admin", pwd="secret")
+        self.open_group_page(wd_helper.wd)
+        self.add_group(wd_helper.wd, Group('','',''))
+        self.return_to_group_page(wd_helper.wd)
+        self.logout(wd_helper.wd)
 
     def test_add_symbols_group(self):
-        wd = self.wd
-        self.open_url(wd, "http://localhost/addressbook/")
-        self.login(wd, login="admin", pwd="secret")
-        self.open_group_page(wd)
-        self.add_group(wd, Group('~!@#$%^&*()_+-=`','~!@#$%^&*()_+-=`','~!@#$%^&*()_+-=`'))
-        self.return_to_group_page(wd)
-        self.logout(wd)
+        wd_helper = self.wd_helper
+        wd_helper.open_url("http://localhost/addressbook/")
+        wd_helper.login(login="admin", pwd="secret")
+        self.open_group_page(wd_helper.wd)
+        self.add_group(wd_helper.wd, Group('~!@#$%^&*()_+-=`','~!@#$%^&*()_+-=`','~!@#$%^&*()_+-=`'))
+        self.return_to_group_page(wd_helper.wd)
+        self.logout(wd_helper.wd)
 
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
@@ -52,34 +54,19 @@ class my_test(unittest.TestCase):
         #click group creation button
         wd.find_element_by_name("new").click()
         #fill group params
-        self.fill_field(wd, field_name='group_name', field_text=group.name)
-        self.fill_field(wd, field_name='group_header', field_text=group.header)
-        self.fill_field(wd, field_name='group_footer', field_text=group.footer)
+        self.wd_helper.fill_field(field_name='group_name', field_text=group.name)
+        self.wd_helper.fill_field(field_name='group_header', field_text=group.header)
+        self.wd_helper.fill_field(field_name='group_footer', field_text=group.footer)
         #click submit button
         wd.find_element_by_name("submit").click()
 
-    def fill_field(self, wd, field_name, field_text):
-        wd.find_element_by_name(field_name).click()
-        wd.find_element_by_name(field_name).clear()
-        wd.find_element_by_name(field_name).send_keys(field_text)
+
 
     def open_group_page(self, wd):
         wd.find_element_by_link_text("groups").click()
 
-    def login(self, wd, login, pwd):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(login)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(pwd)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def open_url(self, wd, url):
-        wd.get(url)
-
     def tearDown(self):
-        self.wd.quit()
+        self.wd_helper.wd.quit()
 
 if __name__ == '__main__':
     unittest.main()
---
+++ wd_helper.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.firefox.webdriver import WebDriver
+
+
+class WDH:
+
+    def __init__(self, wd):
+        self.wd = wd
+
+    def open_url(self, url):
+        self.wd.get(url)
+
+    def login(self, login, pwd):
+        self.wd.find_element_by_name("user").click()
+        self.wd.find_element_by_name("user").clear()
+        self.wd.find_element_by_name("user").send_keys(login)
+        self.wd.find_element_by_name("pass").click()
+        self.wd.find_element_by_name("pass").clear()
+        self.wd.find_element_by_name("pass").send_keys(pwd)
+        self.wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def fill_field(self, field_name, field_text):
+        self.wd.find_element_by_name(field_name).click()
+        self.wd.find_element_by_name(field_name).clear()
+        self.wd.find_element_by_name(field_name).send_keys(field_text)
